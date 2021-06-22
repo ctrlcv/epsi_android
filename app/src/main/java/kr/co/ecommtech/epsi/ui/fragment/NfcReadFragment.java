8b57fb605056bbcfa1aa2d3dd9c6feaa9170ec63
Hyungoo Kim <ctrlcvis@gmail.com>
2021-06-22 17:57:10 +0900

    read NFC 작업

--- app/src/main/java/kr/co/ecommtech/epsi/ui/fragment/NfcReadFragment.java
+++ app/src/main/java/kr/co/ecommtech/epsi/ui/fragment/NfcReadFragment.java
@@ -1,7 +1,41 @@
 package kr.co.ecommtech.epsi.ui.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import org.jetbrains.annotations.NotNull;
+
+import butterknife.ButterKnife;
+import kr.co.ecommtech.epsi.R;
 
 public class NfcReadFragment extends Fragment {
     private static String TAG = "NfcReadFragment";
+
+    @Override
+    public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @org.jetbrains.annotations.Nullable
+    @Override
+    public View onCreateView(@NonNull @NotNull LayoutInflater inflater, @Nullable @org.jetbrains.annotations.Nullable ViewGroup container, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_nfcread, container, false);
+        ButterKnife.bind(this, rootView);
+
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
 }
